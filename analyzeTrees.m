ac942f650249d766b70b0684f5269bec9edded05
Matt Gaidica <matt@gaidi.ca>
2019-07-07 20:51:05 -0700

    Initial commit

---
+++ analyzeTrees.m
@@ -0,0 +1,32 @@
+close all
+
+
+imPath = '/Users/matt/Desktop/cones/IMG_0011.JPG';
+im = imread(imPath);
+[BW,maskedRGBImage] = createTreeMask(im);
+
+BW2 = imfill(BW,'holes');
+
+se = strel('disk',3);
+BW3 = imerode(BW2,se);
+
+[BW4,properties] = filterTreeRegions(BW3);
+
+
+figure;
+imshow(BW4);
+s = regionprops(BW4,'centroid');
+hold on;
+for ii = 1:numel(s)
+    centroid = s(ii).Centroid;
+    text(centroid(1),centroid(2),num2str(ii),'color','r');
+end
+
+figure;
+imshow(im);
+s = regionprops(BW4,'centroid');
+hold on;
+for ii = 1:numel(s)
+    centroid = s(ii).Centroid;
+    text(centroid(1),centroid(2),num2str(ii),'color','r');
+end
